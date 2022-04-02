2d1291cbc03f929a97f2152e897f6365d9bb2f33
karmit1296 <karmit1lalani@gmail.com>
2022-04-02 17:12:19 +0530

    Added fetch data and pagination functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [url, setUrl] = useState(`https://pokeapi.co/api/v2/pokemon`);
+  const [next, setNext] = useState(null);
+  const [previous, setPrevious] = useState(null);
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setPokemon(res.data.results);
+      setNext(res.data.next);
+      setPrevious(res.data.previous);
+    });
+  }, [url]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>List of pokemons</header>
+      <div className="list">
+        {pokemon.map((p) => {
+          return <p className="pokemon">{p.name}</p>;
+        })}
+      </div>
+      <div className="buttons">
+        {previous && (
+          <button className="prev" onClick={() => setUrl(previous)}>
+            Previous
+          </button>
+        )}
+        {next && (
+          <button className="next" onClick={() => setUrl(next)}>
+            Next
+          </button>
+        )}
+      </div>
     </div>
   );
 }
